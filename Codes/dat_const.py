907b18942570c6c124ff480512d3945a0a28c514
Junjieli0704 <lijunjie0704@gmail.com>
2018-05-30 17:26:32 +0800

    Update dat_const.py

--- Codes/dat_const.py
+++ Codes/dat_const.py
@@ -2,8 +2,8 @@
 
 def get_model_type_dict():
     model_type_dict = {}
-    model_type_dict['MHAN'] = {'batch_size': 32}
-    model_type_dict['MHAN_AspSpec'] = {'batch_size': 10}
+    model_type_dict['MODELA'] = {'batch_size': 32}
+    model_type_dict['MODELB'] = {'batch_size': 10}
     return model_type_dict
 
 def get_data_dict():
@@ -18,8 +18,8 @@ def get_data_dict():
             dict['aspect_words'] = '../Data/' + key + '/aspect.words'
 
     data_dict = {}
-    data_dict['TripDMSCMC'] = {'class_num': 5, 'aspect_num': 7}
-    data_dict['tripDatSet'] = {'class_num': 5, 'aspect_num': 7}
+    data_dict['TripDMS'] = {'class_num': 5, 'aspect_num': 7}
+    data_dict['TripOUR'] = {'class_num': 5, 'aspect_num': 7}
 
     get_data_info(data_dict)
 
